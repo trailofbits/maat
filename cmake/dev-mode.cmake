36eb5de74d0e81f98df9bc3c54fd52d89df7072d
Eric Kilmer <eric.d.kilmer@gmail.com>
2022-01-27 10:57:42 -0500

    Remove CMake linting
    Will probably want to add this back in the future

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -19,7 +19,4 @@ if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
   include(cmake/open-cpp-coverage.cmake OPTIONAL)
 endif()
 
-include(cmake/lint-targets.cmake)
-include(cmake/spell-targets.cmake)
-
 add_folders(Project)
