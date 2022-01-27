93ce24cd3213c95c1769eca07ef6127c85673cb0
Eric Kilmer <eric.d.kilmer@gmail.com>
2022-01-27 18:57:04 -0500

    Remove CMake documentation building
    Might want to add this back later

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -5,11 +5,6 @@ if(BUILD_TESTING)
   add_subdirectory(tests)
 endif()
 
-option(BUILD_MCSS_DOCS "Build documentation using Doxygen and m.css" OFF)
-if(BUILD_MCSS_DOCS)
-  include(cmake/docs.cmake)
-endif()
-
 option(ENABLE_COVERAGE "Enable coverage support separate from CTest's" OFF)
 if(ENABLE_COVERAGE)
   include(cmake/coverage.cmake)
--- docs/conf.py.in
+++
@@ -1,6 +0,0 @@
-DOXYFILE = 'Doxyfile'
-
-LINKS_NAVBAR1 = [
-    (None, 'pages', [(None, 'about')]),
-    (None, 'namespaces', []),
-]
