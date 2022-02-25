d7f765892998df9ab4c27d0a8f4c6c3f033331c6
Boyan-MILANOV <boyan.milanov@trailofbits.com>
2022-02-25 12:27:44 +0100

    Explicitly pass sleigh spec dir as env variable for python tests

--- tests/python-tests/run_tests.sh
+++ tests/python-tests/run_tests.sh
@@ -1,5 +1,7 @@
+# Usage: run_tests.sh <maat_python_module.so> <sleigh_spec_dir>
 # Copy maat python module locally
 cp $1 .
+export MAAT_SLEIGH_DIR=$2
 # Run python tests
 python3 -m pytest .
 status=$?
